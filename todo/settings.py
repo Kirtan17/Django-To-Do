d05b3998a6c85bb7c4d712dddcd3d9e51fa3ae42
Kirtan17 <75886245+Kirtan17@users.noreply.github.com>
2021-02-03 19:01:11 +0530

    made changes to settings

--- todo/settings.py
+++ todo/settings.py
@@ -135,6 +135,8 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Add static folder to STATIC_DIRS
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
